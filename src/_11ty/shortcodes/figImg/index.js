d2c4e7177463bf59d77fff7ef498fcfbcd1badaf
Jens Grochtdreis <jens@grochtdreis.de>
2023-04-14 20:58:17 +0200

    Pfad für die Bilder im Shortcode korrigiert

--- src/_11ty/shortcodes/figImg/index.js
+++ src/_11ty/shortcodes/figImg/index.js
@@ -1,6 +1,6 @@
 const figImg = (img, figCaption, alt) => {
   return `<figure class="card">
-    <img src="../../../images/${img}" class="card-img-top" alt="${alt}">
+    <img src="../../../assets/img/${img}" class="card-img-top" alt="${alt}">
     <figcaption class="card-footer">${figCaption}</figcaption>
   </figure>`;
 };
