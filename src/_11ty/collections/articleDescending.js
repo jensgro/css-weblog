4f309c5de9aebcda6ba466954042be2db2dc7912
Jens Grochtdreis <jens@grochtdreis.de>
2025-01-30 00:12:00 +0100

    Listensortierung der Inhalte korrigiert

--- src/_11ty/collections/articleDescending.js
+++ src/_11ty/collections/articleDescending.js
@@ -1,12 +1,11 @@
-// sorting ascending based on title
-// standard is the date.
+// sorting ascending based on date
 // There is no easy api-method.
 // https://github.com/11ty/eleventy/issues/411
 module.exports = function(collection) {
   return collection
     .getFilteredByGlob(["./src/content/article/*.md", "./src/content/article/*.njk"]).sort((a,b) => {
-      if (a.data.title < b.data.title) return -1;
-      else if (a.data.title > b.data.title) return 1;
+      if (a.data.date > b.data.date) return -1;
+      else if (a.data.date < b.data.date) return 1;
       else return 0;
     })
 }
--- src/content/notizen/neues-css-logo.md
+++ src/content/notizen/neues-css-logo.md
@@ -1,6 +1,6 @@
 ---
 title: CSS hat ein neues Logo
-date: 2024-01-02
+date: 2025-01-02
 tags:
     - css
 ---
