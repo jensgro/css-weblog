cb5689077ba1e1bb79c342ea9d21b6fa6592f377
Jens Grochtdreis <jens@grochtdreis.de>
2025-01-29 23:04:41 +0100

    Der SVG-Sprite-Shortcode ist korrigiert

--- src/_11ty/shortcodes/svgSprite/index.js
+++ src/_11ty/shortcodes/svgSprite/index.js
@@ -1,5 +1,9 @@
-const svgSprite = (sprite, icon) => {
-    return `<svg class="svg-icon"><use xlink:href="/assets/svg/${sprite}.svg#${icon}"></use></svg>`;
+const svgSprite = (sprite, icon, modificator = false) => {
+    if(modificator) {
+      return `<svg class="svg-icon ${modificator}"><use xlink:href="/assets/svg/${sprite}.svg#${icon}"></use></svg>`;
+    } else {
+        return `<svg class="svg-icon"><use xlink:href="/assets/svg/${sprite}.svg#${icon}"></use></svg>`;
+    }
 };
   
 module.exports = svgSprite;
